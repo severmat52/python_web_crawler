b2668dc7539a9316835cf3172ceb2dba34388c8f
Matthew Severson <matthew.severson@protonmail.com>
2016-12-16 14:52:49 -0600

    ...

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,7 +1,20 @@
 
 import socket
+from struct import *
 
-s = socket.socket(socket.AF_IPX, socket.SOCK_RAW, socket.IPPROTO_TCP)
+try
+    s = socket.socket(socket.AF_IPX, socket.SOCK_RAW, socket.IPPROTO_TCP)
+except socket.error , msg:
+    print 'Socket could not be created. Error Code : ' str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
 
+# receive a packet
 while True:
-    print(s.recvfrom(65565))
+    packet = s.recvfrom(65565)
+
+    packet = packet[0]
+
+    ip_header = packet[0:20]
+
+    pih = unpack('!BBHHHBBH4s4s', ip_header)
+    
